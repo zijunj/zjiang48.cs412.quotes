3e7e3cb86b9d98d6671a933648af341cfd6285d2
zjiang48 <zjiang48@bu.edu>
2024-09-21 01:32:07 -0400

    Created quotes app

--- quotes/urls.py
+++ quotes/urls.py
@@ -13,8 +13,9 @@ from . import views
 
 urlpatterns = [
     path(r'', views.home_page_view, name="home"),
-    path(r'', views.base_view, name="base"),
-    path("about/", views.about_page_view, name="about"),
+    path(r'quote/', views.quote_page_view, name="quote"),
+    path(r'about/', views.about_page_view, name="about"),
+    path(r'show_all/', views.show_all_page_view, name="show_all"),
 
 ]
 
